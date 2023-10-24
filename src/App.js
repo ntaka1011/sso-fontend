122d946395586d4eea3e2f31b2f2c29031fcb679
unknown <nguyenviethung093@gmail.com>
2023-10-24 17:30:23 +0700

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import About from "./components/About/about";
+import Header from "./components/Header/header";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
